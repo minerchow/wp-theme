67007333f4e5b1f6e0cfb9330695d667b93f5a6e
zhouming <minerchow@126.com>
2024-11-03 16:51:21 +0800

    文章列表

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php get_header(); ?>
-    11
+    <div id="list">
+        <?php 
+            while(have_posts()){
+                the_post();
+                the_title('<h2 class="title"><a target="_blank" href="'.get_permalink().'">','</a></h2>');
+                the_excerpt();
+                echo '<div>'.get_the_author().'</div>';
+                echo '<div>
+                        <span>'.get_the_date('Y-m-d').'</span>
+                        <a target="_blank" href="'.get_permalink().'"><span>查看详情</span></a>
+                      </div>';
+            }
+        ?>
+    </div>
 <?php get_footer(); ?>    
