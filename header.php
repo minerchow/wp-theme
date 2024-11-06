008594034e644bdfe86ea607072e7788218b4166
zhouming <minerchow@126.com>
2024-11-06 21:27:31 +0800

    样式优化

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>">
 </head>
 <body>
     <header>头部</header>
--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php get_header(); ?>
-    <div id="list">
-        <?php 
-            while(have_posts()){
-                the_post();
-                the_title('<h2 class="title"><a target="_blank" href="'.get_permalink().'">','</a></h2>');
-                the_excerpt();
-                echo '<div>'.get_the_author().'</div>';
-                echo '<div>
-                        <span>'.get_the_date('Y-m-d').'</span>
-                        <a target="_blank" href="'.get_permalink().'"><span>查看详情</span></a>
-                      </div>';
-            }
-        ?>
-    </div>
+    <main class="container">
+        <div class="main-box">
+            <div id="list">
+                <?php 
+                    while(have_posts()){
+                        the_post();
+                        echo '<div class="list">';
+                        the_title('<h2 class="title"><a target="_blank" href="'.get_permalink().'">','</a></h2>');
+                        the_excerpt();
+                        echo '<div>'.get_the_author().'</div>';
+                        echo '<div>
+                                <span>'.get_the_date('Y-m-d').'</span>
+                                <a target="_blank" href="'.get_permalink().'"><span>查看详情</span></a>
+                            </div>';
+                        echo '</div>';    
+                    }
+                ?>
+            </div>
+        </div>
+    </main>
+   
 <?php get_footer(); ?>    
